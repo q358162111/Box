31b326b999f9e03a5dca99bde7dc621ff9bd1651
cai_hj <cai_hj@noreply.gitcode.com>
2025-02-08 14:31:50 +0800

    增加检查更新，在线升级功能

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.java
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.java
@@ -1,36 +1,19 @@
 package com.github.tvbox.osc.ui.dialog;
-import android.util.Log;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
 import com.github.tvbox.osc.R;
 
-import com.github.tvbox.osc.util.HawkConfig;
 import com.github.tvbox.osc.util.UpdateChecker;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.util.Objects;
-import com. github. tvbox. osc. util. UpdateChecker;
 import org.jetbrains.annotations.NotNull;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+
 public class AboutDialog extends BaseDialog {
 
     public AboutDialog(@NonNull @NotNull Context context) {
